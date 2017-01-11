7d03f2563b710a48588f08f9106f596eb223f0b6
yangnaihua <yangnaihua.2008@163.com>
2017-01-12 00:06:57 +0100

    主页面简单修改-未完工

--- app/src/main/java/com/helloworld/lyz/allezmap/ProgrammeActivity.java
+++ app/src/main/java/com/helloworld/lyz/allezmap/ProgrammeActivity.java
@@ -1,6 +1,11 @@
 package com.helloworld.lyz.allezmap;
 
+import android.app.AlertDialog;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -111,8 +116,8 @@ public class ProgrammeActivity extends BaseActivity implements NavigationView.On
             case R.id.nav_about:
                 string = "关于";
                 break;
-            case R.id.nav_friend:
-                string = "好友";
+            case R.id.nav_langage:
+                string = "语言";
                 break;
             case R.id.nav_manage:
                 string = "通知";
@@ -153,6 +158,57 @@ public class ProgrammeActivity extends BaseActivity implements NavigationView.On
 
     }
 
+    //首次启动时  检测网络状态
+    @Override
+    protected void onStart() {
+        NetWorkStatus();
+        super.onStart();
+    }
+    // 判断网络连接 方法
+    public void NetWorkStatus() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        connectivityManager.getActiveNetworkInfo();
+
+        boolean netSataus = true;
+        if (connectivityManager.getActiveNetworkInfo() != null) {
+            netSataus = connectivityManager.getActiveNetworkInfo().isAvailable();
+        } else {
+            AlertDialog.Builder aler_builder = new AlertDialog.Builder(this)
+                    .setIcon(R.mipmap.ic_launcher).setTitle(R.string.login_network_title)
+                    .setMessage(R.string.login_network_message);
+            aler_builder.setPositiveButton(R.string.login_network_setting, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    /* Intent mIntent = new Intent("/"); */
+                    /**
+                     * 判断手机系统的版本！如果API大于10 就是3.0+
+                     * 因为3.0以上的版本的设置和3.0以下的设置不一样，调用的方法不同
+                     */
+
+                    Intent intent = null;
+                    /**
+                     * 判断手机系统的版本！如果API大于10 就是3.0+
+                     * 因为3.0以上的版本的设置和3.0以下的设置不一样，调用的方法不同
+                     */
+                    if (android.os.Build.VERSION.SDK_INT > 10) {
+                        intent = new Intent(
+                                android.provider.Settings.ACTION_WIFI_SETTINGS);
+                    } else {
+                        intent = new Intent();
+                        ComponentName component = new ComponentName(
+                                "com.android.settings",
+                                "com.android.settings.WirelessSettings");
+                        intent.setComponent(component);
+                        intent.setAction("android.intent.action.VIEW");
+                    }
+                    startActivity(intent);
+                }
+            }).setNeutralButton(R.string.login_network_cancel, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    dialog.cancel();
+                }
+            }).show();
+        }
+    }
 //    @Override
 //    public void onClick(View v) {
 //        if(v.getId()==R.id.ivAvatar){
